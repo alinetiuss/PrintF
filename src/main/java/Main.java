2ce628471a555ef46f4199da752dced80e2022f6
AlineTiuss <169068304+alinetiuss@users.noreply.github.com>
2024-09-05 01:57:06 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+  public static void main(String[] args) {
+    
+  String nome = "Aline";
+    int dia = 29;
+    int mes = 5;
+    int ano = 2009;
+    System.out.printf(" Minha data de nascimento é: %d/%d/%d ", + dia,mes,ano);
+
+    System.out.println(" ");
+    
+    double pi = 3.14159;
+    
+    System.out.printf(" O valor de pi é: %.4f ",pi );
+
+  }
+}
